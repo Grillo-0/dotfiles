f386c54137b6bca24c3ef89c412d4dcd5d2b2083
Arthur Grillo <arthur.grillo@usp.br>
2021-06-16 15:09:53 -0300

    added the fish config files

---
+++ fish/config.fish
@@ -0,0 +1,5 @@
+export PATH="$PATH:$HOME/flutter/bin"
+export PATH="$PATH:$HOME/android-studio/bin"
+export PATH="$PATH:$HOME/mongodb-linux-x86_64-ubuntu2004-4.4.6/bin"
+
+set -U fish_prompt_pwd_dir_length 0
