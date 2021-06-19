0cc8ef59d6f63867743636bcab8be4ebc5baa73b
Arthur Grillo <arthur.grillo@usp.br>
2021-06-19 19:52:32 -0300

    Added the clang to PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 export PATH="$PATH:$HOME/flutter/bin"
 export PATH="$PATH:$HOME/android-studio/bin"
 export PATH="$PATH:$HOME/mongodb-linux-x86_64-ubuntu2004-4.4.6/bin"
+export PATH="$PATH:$HOME/.config/coc/extensions/coc-clangd-data/install/12.0.0/clangd_12.0.0/bin"
 
 set -U fish_prompt_pwd_dir_length 0
