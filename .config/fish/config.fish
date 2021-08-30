a9327e70f2cc358601e524999cc6d3fa5589cf62
Arthur Grillo <arthur.grillo@usp.br>
2021-08-30 13:46:30 -0300

    changed coloscheme to gruvbox

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,5 @@ export PATH="$PATH:$HOME/flutter/bin"
 export PATH="$PATH:$HOME/android-studio/bin"
 export PATH="$PATH:$HOME/.config/coc/extensions/coc-clangd-data/install/12.0.0/clangd_12.0.0/bin"
 
+theme_gruvbox dark soft
 set -U fish_prompt_pwd_dir_length 0
