e9a7488a7dd248a16760ffa5ccd1c8360fc32699
Arthur Grillo <arthurgrillo@riseup.net>
2024-10-07 22:15:44 -0300

    more conffiiiiiig

---
+++ nvim/.config/nvim/after/plugin/colorscheme.lua
@@ -0,0 +1,13 @@
+require("gruvbox").setup({
+	overrides = {
+		SignColumn = { link = "Normal" },
+		GruvboxGreenSign = { bg = "" },
+		GruvboxOrangeSign = { bg = "" },
+		GruvboxPurpleSign = { bg = "" },
+		GruvboxYellowSign = { bg = "" },
+		GruvboxRedSign = { bg = "" },
+		GruvboxBlueSign = { bg = "" },
+		GruvboxAquaSign = { bg = "" },
+	},
+})
+vim.cmd("colorscheme gruvbox")
